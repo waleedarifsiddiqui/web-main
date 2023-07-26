2eb3b0402efd8e83fd0995a748aca214558bb1ab
Waleed <waleedarifsiddiqui@gmail.com>
2023-07-26 14:27:57 +0500

    .

--- sections/Contact.js
+++ sections/Contact.js
@@ -12,6 +12,7 @@ import { BiUserCircle } from "react-icons/bi";
 import { BsFacebook } from "react-icons/bs";
 import { FiHeadphones, FiHelpCircle } from "react-icons/fi";
 import { IoLocationOutline } from "react-icons/io5";
+import { toast } from "react-hot-toast";
 
 const Contact = () => {
   const form = useRef();
@@ -29,9 +30,11 @@ const Contact = () => {
       .then(
         (result) => {
           console.log(result.text);
+          toast.success('Email successfully sent!')
         },
         (error) => {
           console.log(error.text);
+          toast.error("This didn't work. Please try again.")
         }
       );
     e.target.reset();
